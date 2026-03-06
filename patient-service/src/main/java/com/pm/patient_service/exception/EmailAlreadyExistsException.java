c79f65f2a933669fa5c8b496477ca4b27dbe0ad4
SafwanIrfan <mohdsafwan2k5@gmail.com>
2026-03-06 16:03:44 +0500

    feat: fix patient-service directory issues and add gRPC proto configuration

--- patient-service/src/main/java/com/pm/patient_service/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.pm.patient_service.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
---
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -0,0 +1,18 @@
+package com.pm.patientservice.repository;
+
+import com.pm.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+    boolean existsByEmail(String email);
+
+    // it checks that patient email is in the database
+    // but with different id then it will return true and
+    // error should be thrown.
+    boolean existsByEmailAndIdNot(String email, UUID id);
+}
