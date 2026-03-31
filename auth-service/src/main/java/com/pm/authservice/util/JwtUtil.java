6749b15ada25cfbc7501a1b3ab97513e6cea3a71
SafwanIrfan <mohdsafwan2k5@gmail.com>
2026-03-31 22:58:20 +0500

    fix: resolve docker image build error in jwtUtil and add login test endpoint

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -18,7 +18,7 @@ public class JwtUtil {
 
     private final Key secretKey;
 
-    public JwtUtil(@Value("jwt.secret") String secret) {
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
         byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(
                 StandardCharsets.UTF_8));
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
